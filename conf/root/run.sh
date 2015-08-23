9ba21340808a02b7d4c2d73f5f04734a65388d48
Nesterov Alexander <nesterov@greensight.tu>
2015-08-24 00:08:53 +0300

    update run script

--- conf/root/run.sh
+++ conf/root/run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo $type > /root/test.log
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-availble/$type /etc/nginx/sites-enabled/default
 service mysqld start
 service php56-php-fpm start
 service varnish start
 service nginx start
-chown -R nginx:nginx /var/www
+#chown -R nginx:nginx /var/www
 /bin/bash
